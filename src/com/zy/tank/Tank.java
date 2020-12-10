6fd5a2199dfc298dbcda228c6b4cdd90ec8f1dd2
Holase <zhongyan.zhang@ucdconnect.ie>
2020-12-10 21:19:36 +0800

    增加坦克静止处理，只要方向键都不按，坦克静止

--- src/com/zy/tank/Tank.java
+++ src/com/zy/tank/Tank.java
@@ -13,6 +13,11 @@ import java.awt.Graphics;
 public class Tank {
 
     private int x, y;
+    private boolean moving = false;
+
+    public void setMoving(boolean moving) {
+        this.moving = moving;
+    }
 
     public void setDir(Dir dir) {
         this.dir = dir;
@@ -30,6 +35,12 @@ public class Tank {
 
     public void paint(Graphics g) {
         g.fillRect(x, y, 50, 50);
+        move();
+
+    }
+
+    private void move() {
+        if (!moving) return;
         switch (dir) {
             case LEFT:
                 x -= SPEED;
